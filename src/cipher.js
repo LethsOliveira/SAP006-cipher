d6552a014ac7a1634fc46996bbf4b17935a81a46
Letícia Oliveira <lethsoliveira89@gmail.com>
2021-06-07 18:48:15 -0300

    comecoJS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,16 @@
 const cipher = {
-  // ...
+  encode: function(offset, string){
+    let result = "";
+    for (var i = 0; i<string.length; i++){
+      let ascNumber = string[i].charCodeAt();
+      if (ascNumber >= 65 && ascNumber <= 90){
+        result += string.fromCharCode(ascNumber+getElementById("numberOfLetters"))
+      }
+    }
+  }
+  decode: function(offset, string){
+    return
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let numerOfLetters = document.getElementById("choosing");
+let encryptText = document.getElementById("encryptForm");
+let decryptText = document.getElementById("decryptForm");
+
+let start = document.getElementById("startButton").click();
+let choosingEncrypt = document.getElementById("encrypt").click();
+let choosingDecrypt = document.getElementById("decrypt").click();
+let choosing = document.getElementById("goButton").click();
+let encryptButton = document.getElementById("encryptButton").click();
+let decrypButton = document.getElementById("decryptButton").click();
+
